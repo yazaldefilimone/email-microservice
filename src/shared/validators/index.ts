db5d812973d91cebd1f1acad5ffed4506eeeeeb1
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-09-29 09:57:45 +0200

    feat: create validation of email

---
+++ src/shared/validators/index.ts
@@ -0,0 +1,5 @@
+export const emailAddressValidate = (props: { email: string }) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(props.email).toLowerCase());
+};
