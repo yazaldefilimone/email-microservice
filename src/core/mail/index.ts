ee2c9f7a305c4c5c8d8f02490b17eaf044e4a66e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-02 23:38:34 +0200

    feat: start of create class of mail service

---
+++ src/core/mail/index.ts
@@ -0,0 +1,22 @@
+import { Email } from "../email";
+import { IMail } from "./ports";
+
+export class Mail implements IMail {
+  public sourceAddress: Email;
+  public destinationAddress: Email;
+  public messageTitle: string;
+  public messageBody: string;
+  public replyToAddress?: Email;
+
+  private constructor(props: IMail) {
+    this.sourceAddress = props.sourceAddress;
+    this.destinationAddress = props.destinationAddress;
+    this.messageTitle = props.messageTitle;
+    this.messageBody = props.messageBody;
+    this.replyToAddress = props.replyToAddress;
+  }
+
+  public build(props: IMail) {
+    new Mail(props);
+  }
+}
