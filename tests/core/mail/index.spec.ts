02a556c1790087e13caf46b707d228a1df93a750
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-04 14:00:11 +0200

    refactor: add sender and recipient emails of sut

--- tests/core/mail/index.spec.ts
+++ tests/core/mail/index.spec.ts
@@ -1,27 +1,37 @@
 import { Mail } from "~/core/mail";
 import { Email } from "~/core/email";
 import { IMail } from "./ports";
+import { InvalidParamError } from "~/errors";
 
 const makeSut = () => {
   const mail = Mail;
-  const emailCorrect = {
-    correct: "yazaldefilimon@gmail.com",
-    correctSecund: "jhoedoe@gmail.com",
+
+  const emails = {
+    incorrect: {
+      emailFirst: "jhoedoe@mail",
+      emailSecund: "yazaldefilimon.gmail.com",
+    },
+    correct: {
+      emailFirst: "yazaldefilimon@gmail.com",
+      emailSecund: "jhoedoe@gmail.com",
+    },
   };
+  const sender = Email.build({ email: emails.correct.emailFirst });
+  const recipient = Email.build({ email: emails.correct.emailSecund });
   return {
     mail,
-    emailCorrect,
+    emails,
+    sender,
+    recipient,
   };
 };
 
 describe("Mail", () => {
   it("Should return an content mail all data will is correct", () => {
-    const { mail, emailCorrect } = makeSut();
-    const sender = Email.build({ email: emailCorrect.correct });
-    const recipient = Email.build({ email: emailCorrect.correctSecund });
+    const { mail, emails, sender, recipient } = makeSut();
     if (sender.isLeft() || recipient.isLeft()) {
       console.error("Email return error");
-      throw new Error("Email return error");
+      return null;
     }
     const mailParams: IMail = {
       sourceAddress: sender.value,
