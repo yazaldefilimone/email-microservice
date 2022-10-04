a3774f3189d18ef497966896ad05305a844919a2
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-04 04:53:02 +0200

    feat: create object value of sourceAddress

--- src/core/mail/index.ts
+++ src/core/mail/index.ts
@@ -1,12 +1,13 @@
 import { Email } from "~/core/email";
+import { Either, left, right } from "~/shared/error-handler/either";
 import { IMail } from "./ports";
 
 export class Mail implements IMail {
-  public sourceAddress: Email;
-  public destinationAddress: Email;
-  public messageTitle: string;
-  public messageBody: string;
-  public replyToAddress?: Email;
+  public readonly sourceAddress: Email;
+  public readonly destinationAddress: Email;
+  public readonly messageTitle: string;
+  public readonly messageBody: string;
+  public readonly replyToAddress?: Email;
 
   private constructor(props: IMail) {
     this.sourceAddress = props.sourceAddress;
@@ -15,6 +16,15 @@ export class Mail implements IMail {
     this.messageBody = props.messageBody;
     this.replyToAddress = props.replyToAddress;
   }
+
+  public isSourceAddress(props: { sourceAddress: Email }): Either<Error, Email> {
+    const { sourceAddress } = props;
+    return sourceAddress instanceof Email ? right(props.sourceAddress) : left(new Error("Invalid source address"));
+  }
+
+  public isDestinationAddress(props: { destinationAddress: string }): Either<Error, string> {}
+  public isMessageTitle(props: { messageTitle: string }): Either<Error, string> {}
+  public isMessageBody(props: { messageBody: string }): Either<Error, string> {}
 
   public build(props: IMail) {
     new Mail(props);
