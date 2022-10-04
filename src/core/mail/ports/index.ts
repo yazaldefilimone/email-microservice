d23dd7a6fcc547cec6082abfed44af03399aacb9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-04 05:15:03 +0200

    feat: create types of mail

--- src/core/mail/ports/index.ts
+++ src/core/mail/ports/index.ts
@@ -1,4 +1,6 @@
 import { Email } from "~/core/email";
+import { InvalidParamError } from "~/errors";
+import { Either } from "~/shared/error-handler/either";
 
 export interface IMail {
   sourceAddress: Email;
@@ -7,3 +9,8 @@ export interface IMail {
   messageBody: string;
   replyToAddress?: Email;
 }
+
+export type MailBuildSuccess<T> = T;
+export type MailBuildFailed = InvalidParamError;
+
+export type MailBuildResponse<T> = Either<MailBuildFailed, MailBuildSuccess<T>>;
